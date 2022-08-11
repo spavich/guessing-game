5074a6ae4f7db2e9fba255d059a4aad2e46af2fa
Sasha Paraskevych <sasha.paraskevych@gmail.com>
2022-08-11 14:16:19 +0300

    feat: completed the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+			this.min = min;
+			this.max = max;
     }
 
     guess() {
-
+			this.binSearch = Math.round((this.min + this.max) / 2);
+			return this.binSearch;
     }
 
     lower() {
-
+			this.max = this.guess();
     }
 
     greater() {
-
+			this.min = this.guess();
     }
 }
-
 module.exports = GuessingGame;
